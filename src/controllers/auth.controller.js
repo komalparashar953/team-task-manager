d089ec183b80033be6218106186a2e8840eaf775
komalparashar953 <118298862+komalparashar953@users.noreply.github.com>
2026-05-03 17:02:07 +0530

    Change cookie SameSite attribute from 'strict' to 'none'

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -69,7 +69,7 @@ export const login = asyncHandler(async (req, res) => {
     const options = {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
+        sameSite: 'none',
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     };
 
